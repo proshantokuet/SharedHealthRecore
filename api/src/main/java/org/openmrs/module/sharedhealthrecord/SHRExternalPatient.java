255d68adced64f757c6910c6f06a35312e38f491
jysan-aziz <jeexan2@gmail.com>
2019-12-03 19:40:32 +0600

    shr external patient table + class created

---
+++ api/src/main/java/org/openmrs/module/sharedhealthrecord/SHRExternalPatient.java
@@ -0,0 +1,51 @@
+package org.openmrs.module.sharedhealthrecord;
+
+import java.io.Serializable;
+
+import org.openmrs.BaseOpenmrsObject;
+
+public class SHRExternalPatient extends BaseOpenmrsObject implements Serializable  {
+	 private static final long serialVersionUID = 1L;
+	 private int epid;
+	 private String patient_uuid;
+	 private String is_send_to_central;
+	 
+	 
+	public int getEpid() {
+		return epid;
+	}
+
+	public void setEpid(int epid) {
+		this.epid = epid;
+	}
+
+	public String getPatient_uuid() {
+		return patient_uuid;
+	}
+
+	public void setPatient_uuid(String patient_uuid) {
+		this.patient_uuid = patient_uuid;
+	}
+
+	public String getIs_send_to_central() {
+		return is_send_to_central;
+	}
+
+	public void setIs_send_to_central(String is_send_to_central) {
+		this.is_send_to_central = is_send_to_central;
+	}
+
+
+	@Override
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setId(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ api/src/main/java/org/openmrs/module/sharedhealthrecord/api/SHRExternalPatientService.java
@@ -0,0 +1,8 @@
+package org.openmrs.module.sharedhealthrecord.api;
+
+import org.openmrs.api.OpenmrsService;
+import org.springframework.transaction.annotation.Transactional;
+@Transactional
+public interface SHRExternalPatientService extends OpenmrsService{
+
+}
