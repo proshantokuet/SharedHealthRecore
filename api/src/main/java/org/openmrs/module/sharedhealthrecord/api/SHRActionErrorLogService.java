3c64e2b0bdf634ca9bf1e6961ea0fe9f2f439a41
jysan-aziz <jeexan2@gmail.com>
2019-12-03 16:56:49 +0600

    server data sync - 1 model class and table creation

---
+++ api/src/main/java/org/openmrs/module/sharedhealthrecord/api/SHRActionErrorLogService.java
@@ -0,0 +1,9 @@
+package org.openmrs.module.sharedhealthrecord.api;
+
+import org.openmrs.api.OpenmrsService;
+import org.springframework.transaction.annotation.Transactional;
+@Transactional
+
+public interface SHRActionErrorLogService extends OpenmrsService{
+
+}
