d30be0da338d156eedbff1241084292bec1c2e0b
tanvirmpower <tanvir@mpower-social.com>
2020-03-25 19:48:50 +0600

    DHIS2 json formatting

--- api/src/test/java/org/openmrs/module/sharedhealthrecord/api/ObservationServiceTest.java
+++ api/src/test/java/org/openmrs/module/sharedhealthrecord/api/ObservationServiceTest.java
@@ -14,12 +14,19 @@
 package org.openmrs.module.sharedhealthrecord.api;
 
 import static org.junit.Assert.assertNotNull;
+import groovy.ui.Console;
 
 import java.io.FileReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.ClientAnchor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -37,12 +44,22 @@ import org.springframework.test.web.servlet.result.PrintingResultHandler;
 
 import ca.uhn.hl7v2.model.v25.segment.IIM;
 
-import com.google.gson.Gson;
+import com.google.gson.Gson;import com.google.gson.JsonObject;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+
 
 /**
  * Tests {@link $ SharedHealthRecordService} .
  */
 public class ObservationServiceTest extends BaseModuleContextSensitiveTest {
+	
+	private final static String baseOpenmrsUrl = "https://192.168.19.145";
+	
+	private final static String globalServerUrl = "https://bahmni.mpower-social.com";
+	
+	private static String formsToFilter = "";
+	
 	
 	@Test
 	public void shouldSetupContext() {
@@ -51,75 +68,51 @@ public class ObservationServiceTest extends BaseModuleContextSensitiveTest {
 	
 	JSONParser jsonParser = new JSONParser();
 	
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "unused", "unused" })
 	@Test
 	public void encounter() {
-		
 		JSONParser jsonParser = new JSONParser();
-		
-			try {
-	        
-			String patientVisitUrl = "https://192.168.19.145/openmrs/ws/rest/v1/visit?includeInactive=true&patient=8c5259dc-08a9-46fa-9217-c9b6f7b7b78d&v=custom:(encounters:(uuid))";
-			String patientVisitResponse = HttpUtil.get(patientVisitUrl, "", "admin:test");
-			
-			JSONObject patientVisitObject = (JSONObject) jsonParser.parse(patientVisitResponse);
-			JSONArray visitsArray = (JSONArray) patientVisitObject.get("results");
-		    visitsArray.forEach(_ob -> {
-				JSONObject encountersObject = (JSONObject) _ob;
-				JSONArray encountersArray = (JSONArray) encountersObject.get("encounters");
-				encountersArray.forEach(_enc -> {
-					JSONObject singleEncountersObject = (JSONObject) _enc;
-					String encounterUuid = (String) singleEncountersObject.get("uuid");
-					String getEncounterUrl = "https://192.168.19.145/openmrs/ws/rest/v1/bahmnicore/bahmniencounter/"+encounterUuid+"?includeAll=true";
-					String patientencounterResponse = HttpUtil.get(getEncounterUrl, "", "admin:test");
-					System.out.println("patientencounterResponse" + patientencounterResponse);
-
-					// System.out.println("json parse starting...........");
-					JSONObject obj;
-
-					try {
-						obj = (JSONObject) jsonParser.parse(patientencounterResponse);
-//						String visitUUIdString = (String)obj.get("visitUuid");
-//						String visitDetailsByVIsitUuidURL = "https://192.168.19.145/openmrs/ws/rest/v1/visit/"+visitUUIdString+"?includeAll=true";
-//						String visitDetailsByVIsitUuid = HttpUtil.get(visitDetailsByVIsitUuidURL, "", "admin:test");
-//						JSONObject visitObject = (JSONObject) jsonParser.parse(visitDetailsByVIsitUuid);
-//						JSONObject visitStartJsonObject = new JSONObject();
-//						visitStartJsonObject.put("visitType", visitObject.get("visitType"));
-//						visitStartJsonObject.put("patient", visitObject.get("patient"));
-//						visitStartJsonObject.put("startDatetime", visitObject.get("startDatetime"));
-//						visitStartJsonObject.put("stopDatetime", visitObject.get("stopDatetime"));
-//						String visitSavingUrl = "https://192.168.19.147/openmrs/ws/rest/v1/visit";
-//						String visitSavingResponse = HttpUtil.post(visitSavingUrl, "", visitStartJsonObject.toString());
+		Boolean postResponseOfService = true ;
+		try {
+				String getEncounterUrl = globalServerUrl +"/openmrs/ws/rest/v1/bahmnicore/bahmniencounter/4ff2410b-7552-464d-9db2-e3d291754c29?includeAll=true";
+				String patientencounterResponse = HttpUtil.get(getEncounterUrl, "", "admin:test");
+				JSONObject EncounterObj;
+				try {
+					JSONObject servicesToPost = new JSONObject();
+					EncounterObj = (JSONObject) jsonParser.parse(patientencounterResponse);
+					JSONArray obs = (JSONArray) EncounterObj.get("observations");
+					JSONArray IntialJsonDHISArray =  getObservations(obs);
+					Object document = Configuration.defaultConfiguration().jsonProvider().parse(IntialJsonDHISArray.toString());
+					List<String> servicesInObservation = JsonPath.read(document, "$..service");
+					Set<String> uniqueSetOfServices = new HashSet<>(servicesInObservation);
+					System.out.println(uniqueSetOfServices.toString());
+					uniqueSetOfServices.forEach(uniqueSetOfService ->{
+						List<String> extractServiceJSON = JsonPath.read(document, "$.[?(@.service == '"+uniqueSetOfService+ "')]");
+					      String jsonStr = JSONArray.toJSONString(extractServiceJSON);
+							try {
+								JSONArray extractServiceArray = (JSONArray) jsonParser.parse(jsonStr);
+								extractServiceArray.forEach(service -> {
+									JSONObject serviceObject = (JSONObject) service;
+									String elementId = ObserVationDHISMapping.get(serviceObject.get("question"));
+									serviceObject.put("elementId", elementId);
+								});
+								servicesToPost.put(uniqueSetOfService, extractServiceArray);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
 						
-//						String visitSavingResponse = createVisit(obj);
-//						JSONObject visitJsonAfterSaving = (JSONObject) jsonParser.parse(visitSavingResponse);
-//						System.out.println("visitJsonAfterSaving" + visitJsonAfterSaving);
-//						String visitUuid = (String) visitJsonAfterSaving.get("uuid");
-//						String visitTypeUuidString = (String)obj.get("visitTypeUuid");
-//						String visitTypeValue = visitTypeMapping.get(visitTypeUuidString);
-//						obj.put("visitType", visitTypeValue);
-//						obj.remove("visitUuid");
-//						obj.put("visitUuid", visitUuid);
-						JSONArray obs = (JSONArray) obj.get("observations");
-						JSONArray obervations = getObservations(obs);
-						JSONObject encounter = (JSONObject) jsonParser.parse(new Gson().toJson(new Gson().fromJson(obj.toString(),Encounter.class)));
-						encounter.put("observations", obervations);
-						System.out.println("parsed Json :" + encounter.toJSONString());
-						String patientServiceUrl = "https://192.168.19.147/openmrs/ws/rest/v1/bahmnicore/bahmniencounter";
-						// String postResponse = HttpUtil.post(patientServiceUrl, "", encounter.toJSONString());
-						//System.out.println("After Post Request Response :" + postResponse.toString());
-						
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				});
-		    });
-			
+					});
+					System.out.println(servicesToPost.toString());
+					//System.out.println(servicesToPost.size());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
+			postResponseOfService = false;
 		}
+
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -129,26 +122,66 @@ public class ObservationServiceTest extends BaseModuleContextSensitiveTest {
 		
 		_obs.forEach(_ob -> {
 			JSONObject ob = (JSONObject) _ob;
-			
 			String type = (String) ob.get("type");
 			JSONArray groupMembers = (JSONArray) ob.get("groupMembers");
 			// System.out.println("Coded..........:" + groupMembers.size() + " type:" + type);
 			try {
 				if (!StringUtils.isBlank(type) && type.equalsIgnoreCase("Coded")) {
-					
-					JSONObject obs = (JSONObject) jsonParser.parse(new Gson().toJson(new Gson().fromJson(ob.toString(),
-					    ObservationWithValues.class)));
-					observations.add(obs);
+					JSONObject codedConceptJsonObject = new JSONObject();
+					String serviceName = (String) ob.get("formFieldPath");
+					String serviceSplit[] = serviceName.split("\\.");
+					serviceName = serviceSplit[0];
+					JSONObject conceptJsonObject = (JSONObject) ob.get("concept");
+					String questionName = (String) conceptJsonObject.get("name");
+					String answerValue = (String) ob.get("valueAsString");
+					codedConceptJsonObject.put("question", questionName);
+					if(isNumeric(answerValue)) {
+						codedConceptJsonObject.put("answer", Double.parseDouble(answerValue));
+					}
+					else {
+						codedConceptJsonObject.put("answer", answerValue);
+					}
+					codedConceptJsonObject.put("service", serviceName);
+					observations.add(codedConceptJsonObject);
+
 				} else if (groupMembers.size() != 0) {
+					groupMembers.forEach(_groupMember -> {
+						JSONObject groupMember = (JSONObject) _groupMember; 
+						JSONObject codedConceptJsonObject = new JSONObject();
+						String serviceName = (String) groupMember.get("formFieldPath");
+						String serviceSplit[] = serviceName.split("\\.");
+						serviceName = serviceSplit[0];
+						JSONObject conceptJsonObject = (JSONObject) groupMember.get("concept");
+						String questionName = (String) conceptJsonObject.get("name");
+						String answerValue = (String) groupMember.get("valueAsString");
+						codedConceptJsonObject.put("question", questionName);
+						if(isNumeric(answerValue)) {
+							codedConceptJsonObject.put("answer", Double.parseDouble(answerValue));
+						}
+						else {
+							codedConceptJsonObject.put("answer", answerValue);
+						}
+						codedConceptJsonObject.put("service", serviceName);
+						observations.add(codedConceptJsonObject);
+					});
 					
-					JSONObject obs = (JSONObject) jsonParser.parse(new Gson().toJson(new Gson().fromJson(ob.toString(),
-					    ObservationWithGroupMemebrs.class)));
-					observations.add(obs);
 				} else {
-					
-					JSONObject obs = (JSONObject) jsonParser.parse(new Gson().toJson(new Gson().fromJson(ob.toString(),
-					    Observation.class)));
-					observations.add(obs);
+					JSONObject codedConceptJsonObject = new JSONObject();
+					String serviceName = (String) ob.get("formFieldPath");
+					String serviceSplit[] = serviceName.split("\\.");
+					serviceName = serviceSplit[0];
+					JSONObject conceptJsonObject = (JSONObject) ob.get("concept");
+					String questionName = (String) conceptJsonObject.get("name");
+					String answerValue = (String) ob.get("valueAsString");
+					codedConceptJsonObject.put("question", questionName);
+					if(isNumeric(answerValue)) {
+						codedConceptJsonObject.put("answer", Double.parseDouble(answerValue));
+					}
+					else {
+						codedConceptJsonObject.put("answer", answerValue);
+					}
+					codedConceptJsonObject.put("service", serviceName);
+					observations.add(codedConceptJsonObject);
 				}
 				
 			}
@@ -158,31 +191,41 @@ public class ObservationServiceTest extends BaseModuleContextSensitiveTest {
 				e.printStackTrace();
 			}
 		});
+		//System.out.println(observations.toJSONString());
 		return observations;
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static String createVisit (JSONObject obj) {
+	public static Boolean createVisit (JSONObject obj, String patientUuid) throws Exception {
 		
-		String visitSavingResponse = "";
+		Boolean visitSavingResponse = false;
 		JSONParser jsonParser = new JSONParser();
 		
 		try {
 			
 			String visitUUIdString = (String)obj.get("visitUuid");
-			String visitDetailsByVIsitUuidURL = "https://192.168.19.145/openmrs/ws/rest/v1/visit/"+visitUUIdString+"?includeAll=true";
+			String visitDetailsByVIsitUuidURL = globalServerUrl + "/openmrs/ws/rest/v1/save-Patient/search/patientVisitByUuid?visit_uuid=" + visitUUIdString;
 			String visitDetailsByVIsitUuid = HttpUtil.get(visitDetailsByVIsitUuidURL, "", "admin:test");
 			JSONObject visitObject = (JSONObject) jsonParser.parse(visitDetailsByVIsitUuid);
-			JSONObject visitStartJsonObject = new JSONObject();
-			visitStartJsonObject.put("visitType", visitObject.get("visitType"));
-			visitStartJsonObject.put("patient", visitObject.get("patient"));
-			visitStartJsonObject.put("startDatetime", visitObject.get("startDatetime"));
-			visitStartJsonObject.put("stopDatetime", visitObject.get("stopDatetime"));
-			String visitSavingUrl = "https://192.168.19.147/openmrs/ws/rest/v1/visit";
-			// visitSavingResponse = HttpUtil.post(visitSavingUrl, "", visitStartJsonObject.toString());
-			
+//			JSONObject visitStartJsonObject = new JSONObject();
+//			visitStartJsonObject.put("visitType", visitObject.get("visitType"));
+//			visitStartJsonObject.put("patient", visitObject.get("patient"));
+//			visitStartJsonObject.put("startDatetime", visitObject.get("startDatetime"));
+//			visitStartJsonObject.put("stopDatetime", visitObject.get("stopDatetime"));
+			visitObject.put("patient_uuid", patientUuid);
+			visitObject.remove("isFound");
+			String visitSavingUrl = baseOpenmrsUrl + "/openmrs/ws/rest/v1/save-Patient/insert/patientVisitDetails";
+			String visitSavingResponseString = HttpUtil.post(visitSavingUrl, "", visitObject.toString());
+			JSONObject visitSavingObject = (JSONObject) jsonParser.parse(visitSavingResponseString);
+			if(visitSavingObject.containsKey("isSuccessfull")) {
+				Boolean isSuccessfull =  (Boolean)visitSavingObject.get("isSuccessfull");
+				if(isSuccessfull) {
+					visitSavingResponse = true;
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			visitSavingResponse = false;
 		}
 		return visitSavingResponse;
 	}
@@ -192,6 +235,17 @@ public class ObservationServiceTest extends BaseModuleContextSensitiveTest {
         visitTypeMapping.put("c228eab1-3f10-11e4-adec-0800271c1b75", "IPD");
         visitTypeMapping.put("c22a5000-3f10-11e4-adec-0800271c1b75", "OPD");
         visitTypeMapping.put("bef32e14-3f12-11e4-adec-0800271c1b75", "LAB VISIT");
+	}
+	
+	public static final Map<String, String> ObserVationDHISMapping = new HashMap<String, String>();
+	static {
+		ObserVationDHISMapping.put("History of Past Illness", "UXgONLVlRlA");
+		ObserVationDHISMapping.put("Family History", "NOOR11jHbxK");
+		ObserVationDHISMapping.put("Chief Complaint", "d49IcANqJh9");
+	}
+	
+	public static boolean isNumeric(String str) {
+		return str.matches("[0-9.]*");
 	}
 	
 }
